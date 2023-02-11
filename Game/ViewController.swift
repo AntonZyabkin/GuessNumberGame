3203665bdac9b0d7d03d3eab443a8853817c6966
Anatan <zyabkinantonv@gmail.com>
2023-02-11 10:06:45 +0300

    create project

---
+++ Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Game
+//
+//  Created by Anton Zyabkin on 11.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
